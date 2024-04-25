6ebdc06b0d79d0285baa4d5e1f91defe11d41bde
KleyversonO <kleyversonoliveira@gmail.com>
2024-04-25 08:27:35 -0300

    Modificação das anotações do javascript

--- login.js
+++ login.js
@@ -3,23 +3,22 @@ const emailInput = document.querySelector('.formLogin input[type="email"]');
 const passwordInput = document.querySelector('.formLogin input[type="password"]');
 
 form.addEventListener('submit', (event) => {
-  event.preventDefault(); // Prevent default form submission
+  event.preventDefault(); 
 
   const email = emailInput.value.trim();
   const password = passwordInput.value.trim();
 
   if (!email || !password) {
     alert('Por favor, preencha os campos de e-mail e senha.');
-    // Focus on the first empty field (email by default)
+
     if (!email) {
       emailInput.focus();
     } else {
       passwordInput.focus();
     }
-    return; // Prevent form submission if either field is empty
+    return;
   }
 
-  // If both fields are filled, proceed with form submission or other logic (replace with your actual logic)
   console.log('Formulário enviado com email:', email, 'e senha:', password);
 
   window.location.href = "logado.html";
